a467fdb61c859805192e0ace9e5e746b845a1d54
Robert Moritz <robert@nott-studios.co.uk>
2025-10-26 16:02:30 +0000

    revert: back to bun file

--- main.ts
+++ main.ts
@@ -1,4 +1,3 @@
-import notebookPen from "./src/assets/notebook-pen.svg";
 import homePage from "./src/pages/(root)/index.html";
 import ApiGET from "./src/pages/api/get";
 import ApiPOST from "./src/pages/api/post";
@@ -10,12 +9,15 @@ Bun.serve({
 	routes: {
 		"/": homePage,
 		"/notes": notesPage,
-		"/notebook-pen.svg": new Response(notebookPen, {
-			headers: {
-				"Content-Type": "image/svg+xml",
-				"Access-Control-Allow-Origin": "https://www.owlbear.rodeo",
+		"/notebook-pen.svg": new Response(
+			await Bun.file("./src/assets/notebook-pen.svg").bytes(),
+			{
+				headers: {
+					"Content-Type": "image/svg+xml",
+					"Access-Control-Allow-Origin": "https://www.owlbear.rodeo",
+				},
 			},
-		}),
+		),
 		"/manifest": (req) => {
 			const url = new URL(req.url);
 			return Response.json(
